8d93dd53d250dd9b7b91870208f5176e88d2e585
Deep Rathod <rathoddeep516@gmail.com>
2022-03-15 14:08:43 +0530

    issue resolution

--- src/test/java/MyFirstClass.java
+++ src/test/java/MyFirstClass.java
@@ -7,23 +7,24 @@ import org.testng.annotations.Test;
 
 public class MyFirstClass {
     public String baseUrl = "http://demo.guru99.com/test/newtours/";
-    String driverPath = "C:\\geckodriver.exe";
+    String driverPath = "C:\\chromedriver.exe";
     public WebDriver driver ;
-    @AfterTest                            //Jumbled
-    public void terminateBrowser(){
-        driver.close();
-    }
+
     @BeforeTest                            //Jumbled
     public void launchBrowser() {
-        System.out.println("launching firefox browser");
-        System.setProperty("webdriver.gecko.driver", driverPath);
+        System.out.println("launching chrome browser");
+        System.setProperty("webdriver.chrome.driver", driverPath);
         driver = new ChromeDriver();
         driver.get(baseUrl);
     }
-    @Test                              //Jumbled
+    @Test                             //Jumbled
     public void verifyHomepageTitle() {
         String expectedTitle = "Welcome: Mercury Tours";
         String actualTitle = driver.getTitle();
         Assert.assertEquals(actualTitle, expectedTitle);
+    }
+    @AfterTest                            //Jumbled
+    public void terminateBrowser(){
+        driver.close();
     }
 }
